ace06fd477cbcd227790ceb3864b34ab15cfa752
xJosafat <iwant_to_believe@hotmail.com>
2015-10-08 15:24:16 -0500

    Broadcast a message when someone connects or disconnects

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  //console.log('jsa');
-  res.sendFile('/Users/Josafat/Desktop/learn/socket-io-chat/index.html');
+	//console.log('jsa');
+	res.sendFile('/Users/Josafat/Desktop/learn/socket-io-chat/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-	console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-	io.emit('chat message', msg);
-  });
+	io.emit('chat message', "a user connected");
+	socket.on('disconnect', function(){
+		io.emit('chat message', "user disconnected");
+	});
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
